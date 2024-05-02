84a2d9cb6bec034c78a003b654bec3fd402a8877
hooooooooooooon <douglaskim91@gmail.com>
2024-05-02 17:01:07 +0900

    States

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import styled from "styled-components";
 
-interface CircleProps {
-    bgColor: string;
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
 }
 
-const Container = styled.div<CircleProps>`
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-    return <Container bgColor={bgColor} />;
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+}
+
+function Circle({ bgColor, borderColor }: CircleProps) {
+  const [value, setValue] = useState<string>("");
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
