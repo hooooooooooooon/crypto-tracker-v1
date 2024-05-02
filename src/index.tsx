21ff752c5178889d77a1c14f44068fa2a69f627b
hooooooooooooon <douglaskim91@gmail.com>
2024-05-02 16:11:31 +0900

    Typing the Props

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
-
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
+    <App />
   </React.StrictMode>
 );
 
